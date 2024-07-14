27daa2c4a044ecb75f3551e800d40b14688b90af
Volodymyr Nerovnia <nerv@i.ua>
2024-07-14 23:28:36 +0300

    Ibit and config application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hover1 from './components/images/effects/1-hover/1-hover'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Hover1 />
+    </>
+  )
+}
+
+export default App
