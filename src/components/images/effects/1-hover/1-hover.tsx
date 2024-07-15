ca9477dc3eef8d51daf03f041ebea4a8f7c3ce66
Volodymyr Nerovnia <nerv@i.ua>
2024-07-15 23:22:49 +0300

    Added image hover component

---
+++ src/components/images/effects/1-hover/1-hover.tsx
@@ -0,0 +1,14 @@
+import './img-hover.css'
+
+function Hover1() {
+
+  return (
+    <div>
+      <img src="./src//components/images/effects/1-hover/img/1-img.jpeg" alt="nature" width="400" height="250" />
+      <img src="./src//components/images/effects/1-hover/img/2-img.jpeg" alt="nature" width="400" height="250" />
+      <img src="./src//components/images/effects/1-hover/img/3-img.jpeg" alt="nature" width="400" height="250" />
+    </div>
+  )
+}
+
+export default Hover1;
