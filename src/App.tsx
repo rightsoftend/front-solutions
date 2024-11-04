19b65ce55f1ab126e4e10378ccf580df9f873a89
Volodymyr Nerovnia <nerv@i.ua>
2024-11-04 17:04:22 +0200

    Add SVGComponent component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Hover1 from './components/images/effects/1-hover/1-hover'
 import ModernButton from './components/buttons/modern'
 import './App.css'
 import InputBorderGradient from './components/inputs/border-gradient'
+import SVGComponent from './components/images/svg'
 
 function App() {
   return (
     <div className='flex flex-col'>
+      <SVGComponent />
       <InputBorderGradient />
       <ModernButton />
       <NeonText text="Text"/>
---
+++ src/components/images/svg/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { ReactComponent as Check } from './logo.svg';
+
+const SVGComponent = () => {
+  return (
+    <Check height="218px" width="218px" />
+  );
+}
+
+export default SVGComponent;  
